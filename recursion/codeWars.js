164cd37e99872170a9f5314bfe0668dc3a7f74ce
I-Maged <magedhanafy@gmail.com>
2022-11-10 23:57:25 +0200

    more code wars

--- recursion/codeWars.js
+++ recursion/codeWars.js
@@ -18,3 +18,43 @@ const power = (base, exp) => {
 };
 
 console.log(`power(2, 4) = ${power(2, 4)}`);
+
+/* 
+Write a function that returns the factorial of a number. As a quick refresher, a factorial of a number is the result of that number multiplied by the number before it, and the number before that number, and so on, until you reach 1. The factorial of 1 is just 1.
+*/
+
+const factorial = (num) => {
+  return num <= 1 ? 1 : num * factorial(num - 1);
+};
+
+console.log(`factorial 5 = ${factorial(5)}`);
+
+/* 
+Write a function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function.
+*/
+
+const all = (arr, callback) => {
+  if (arr.length === 0) return true;
+
+  if (callback(arr[0])) {
+    arr.shift();
+    return all(arr, callback);
+  } else {
+    return false;
+  }
+};
+const allAreLessThanSeven = all([1, 2, 9], function (num) {
+  return num < 7;
+});
+
+console.log(`allAreLessThanSeven = ${allAreLessThanSeven}`);
+
+/* 
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all
+*/
+
+const productOfArray = (arr) => {
+  return arr.length == 0 ? 1 : arr.shift() * productOfArray(arr);
+};
+
+console.log(`product Of Array [1,2,3,10] = ${productOfArray([1, 2, 3, 10])}`);
