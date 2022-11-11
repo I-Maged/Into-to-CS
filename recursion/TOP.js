e9bac2c77d5c31443ec5e71d13cb65c9344594ee
I-Maged <magedhanafy@gmail.com>
2022-11-11 20:13:40 +0200

    merge sort

--- recursion/TOP.js
+++ recursion/TOP.js
@@ -20,3 +20,33 @@ const fibsRec = (num, arr = [0, 1]) => {
     : fibsRec(--num, [...arr, arr[arr.length - 1] + arr[arr.length - 2]]);
 };
 console.log(fibsRec(8));
+
+/* 
+Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+*/
+
+const merge = (left, right) => {
+  const sortedArr = [];
+
+  while (left.length && right.length) {
+    left[0] < right[0]
+      ? sortedArr.push(left.shift())
+      : sortedArr.push(right.shift());
+  }
+
+  return [...sortedArr, ...left, ...right];
+};
+
+const mergeSort = (arr) => {
+  const half = Math.floor(arr.length / 2);
+
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const left = arr.splice(0, half);
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+console.log(mergeSort([12, 34, 11, 1, 54, 25, 67, 45]));
